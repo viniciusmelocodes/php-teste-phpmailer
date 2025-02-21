09e558f75f117883e981dcb7af22f086222fd8f7
Vinicius Melo <vinicius.simplicio@artit.com.br>
2025-02-21 17:03:20 -0300

    alteração de configuração para hostgator

--- send-mail.php
+++ send-mail.php
@@ -17,16 +17,16 @@ function sendEmail($email, $subject, $message) {
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host = 'smtp.mail.yahoo.com';                     //Set the SMTP server to send through
+        $mail->Host = 'sh-pro128.hostgator.com.br';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $mail->Username = 'simpliciovinicius@yahoo.com';                     //SMTP username
+        $mail->Username = 'atendimento@atacadaoferragista.com.br';                     //SMTP username
         $mail->Password = $password;                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+        $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
         //Recipients
-        $mail->setFrom('simpliciovinicius@yahoo.com', 'V. Melo');
-        $mail->addAddress($email);     //Add a recipient
+        $mail->setFrom('atendimento@atacadaoferragista.com.br', 'atacadao');
+        $mail->addAddress('hawep17607@noomlocs.com');     //Add a recipient
     
         // $mail->addAddress('ellen@example.com');               //Name is optional
         // $mail->addReplyTo('info@example.com', 'Information');
@@ -49,5 +49,46 @@ function sendEmail($email, $subject, $message) {
     }
 }
 
-$data = json_decode(file_get_contents('php://input'), true);
-sendEmail($data['email'], $data['subject'], $data['message']);
+// $data = json_decode(file_get_contents('php://input'), true);
+// sendEmail($data['email'], $data['subject'], $data['message']);
+
+try {
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    $env = parse_ini_file('.env');
+    $password = $env["PASSWORD"];
+
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'sh-pro128.hostgator.com.br';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = 'atendimento@atacadaoferragista.com.br';                     //SMTP username
+    $mail->Password = $password;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('atendimento@atacadaoferragista.com.br', 'atacadao');
+    $mail->addAddress('hawep17607@noomlocs.com');     //Add a recipient
+
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    $mail->isHTML(true);
+    $mail->Subject = '';
+    $mail->Body = '1';
+    $mail->AltBody = strip_tags('1');
+
+    $mail->send();
+    echo "Message has been sent";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
